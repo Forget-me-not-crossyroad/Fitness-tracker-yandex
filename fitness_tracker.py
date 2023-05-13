633dc83c37f9f943c7518360d0c04a70be44e909
= <=>
2023-05-14 02:28:54 +0300

    code optimization

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -1,13 +1,4 @@
 import datetime as dt
-import randomtimestamp
-
-endDate_1 = dt.time(6, 00, 00)
-endDate_2 = dt.time(12, 00, 00)
-endDate_3 = dt.time(18, 00, 00)
-endDate_4 = dt.time(23, 00, 00)
-
-a = randomtimestamp.random_time()
-# print(a)
 
 HEIGHT = 175
 WEIGHT = 75
@@ -17,7 +8,7 @@ TRANSFER_COEFF = 1000
 K_1 = 0.035
 K_2 = 0.029
 storage_data = {}
-
+calories_list = []
 
 def accept_package(package):
     storage_data_internal = {}
@@ -25,14 +16,11 @@ def accept_package(package):
         storage_data_internal[package[0]] = package[1]
     else:
         return 'Получен поврежденный пакет данных'
-    print(storage_data_internal)
     storage_data.update(storage_data_internal)
     steps = get_step_day(storage_data)
     dist = get_distance(steps)
-    current_time = package[0]
-    current_steps = package[1]
-    print(f'Время: {current_time}/nКоличество шагов за сегодня: {steps}./nДистанция составила {dist} км./nВы сожгли {get_spent_calories(current_steps, current_time)} ккал./n{get_achievement(dist)}')
-
+    calories_list.append(get_spent_calories(package[1], package[0]))
+    show_message(package[0], steps, dist, get_calories_day(calories_list), get_achievement(dist))
 
 def get_achievement(dist):
     if dist >= 6.5:
@@ -47,12 +35,10 @@ def get_achievement(dist):
 
 
 def check_correct_data(data):
-    print(data)
     return True
 
 
 def check_correct_time(time):
-    print(time)
     return True
 
 
@@ -60,20 +46,23 @@ def get_step_day(dict_steps):
     return sum(step for step in dict_steps.values())
 
 
+def get_calories_day(calories_list):
+    return sum(step for step in calories_list)
+
+
 def get_distance(steps):
     return int((steps * STEP_M) / TRANSFER_COEFF)
 
 
 def get_spent_calories(dist, current_time):
-    time_list = list(storage_data.keys())
     if len(storage_data) != 1:
+        time_list = list(storage_data.keys())
         previous_time = time_list[len(storage_data) - 2]
         T = time_interval_calculation(current_time, previous_time)
         V = get_distance(dist) / round(T)
     else:
         T = time_interval_calculation(current_time, '0:00:00')
         V = get_distance(dist) / round(T)
-    print(V)
     return round(K_1 * WEIGHT + (V**2 / HEIGHT) * K_2 * WEIGHT * T * 60)
 
 
@@ -92,10 +81,10 @@ def convert(tup, di):
 
 def show_message(time, steps, dist, calories, achievement):
     print(f'Время: {time}'
-          f'/nКоличество шагов за сегодня: {steps}.'
-          f'/nДистанция составила {dist} км.'
-          f'/nВы сожгли {calories} ккал.'
-          f'/n{achievement}')
+          f'\nКоличество шагов за сегодня: {steps}.'
+          f'\nДистанция составила {dist} км.'
+          f'\nВы сожгли {calories} ккал.'
+          f'\n{achievement}')
 
 
 package2 = ('9:36:02', 15000)
@@ -103,12 +92,14 @@ package2 = ('9:36:02', 15000)
 
 package3 = ('11:00:02', 15000)
 
-package4 = ('13:13:13', 2000)
+
+package4 = ('13:13:13', 15000)
 
 
 accept_package(package2)
 
 
 accept_package(package3)
+
 
 accept_package(package4)
